ef92879b1ac8dd7b7e6046a46138c5ce8cd8bc79
ansh010599 <ansh@ualberta.ca>
2022-02-06 17:04:56 -0700

    Start of conversion from django to python3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from kivy.core.window import Window
 from kivy.properties import StringProperty
 from kivymd.app import MDApp
 
-from backend import get_words
+from backend import get_main_page_results_list
 
 # To update a variable in .kv, you could go self.root.ids.{id}.text = ""
 
@@ -19,11 +19,11 @@ class MainLayout(BoxLayout):
 
     def on_submit_word(self, widget):
         # To get access to the input, you could also go TextinputId.text directly.
-        output_res = get_words(widget.text)
+        output_res = get_main_page_results_list(widget.text)
         x = random.randint(0, 2)
         print(x)
         print(output_res)
-        self.results_print_str = output_res[x][0]
+        self.results_print_str = str(output_res[x])
         # self.root.ids.result_label.text = output_res does the same thing as the above line
 
 
