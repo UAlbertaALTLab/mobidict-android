400ab5a6dc5ac6d5105f19809cd90942871f637f
ansh010599 <ansh@ualberta.ca>
2022-02-06 23:48:20 -0700

    Search with affix translation: #1

---
+++ core/runner.py
@@ -0,0 +1,70 @@
+import re
+
+from CreeDictionary.API.search.affix import (
+    do_source_language_affix_search,
+    do_target_language_affix_search,
+    query_would_return_too_many_results,
+)
+from CreeDictionary.API.search.core import SearchRun
+from CreeDictionary.API.search.espt import EsptSearch
+from CreeDictionary.API.search.lookup import fetch_results
+from CreeDictionary.API.search.query import CvdSearchType
+from CreeDictionary.API.search.types import Result
+from CreeDictionary.API.search.util import first_non_none_value
+
+CREE_LONG_VOWEL = re.compile("[êîôâēīōā]")
+
+
+def search(
+    *, query: str, include_affixes=True, include_auto_definitions=False
+) -> SearchRun:
+    """
+    Perform an actual search, using the provided options.
+
+    This class encapsulates the logic of which search methods to try, and in
+    which order, to build up results in a SearchRun.
+    """
+    search_run = SearchRun(
+        query=query, include_auto_definitions=include_auto_definitions
+    )
+
+    if search_run.query.espt:
+        espt_search = EsptSearch(search_run)
+        espt_search.analyze_query()
+
+    if settings.MORPHODICT_ENABLE_CVD:
+        cvd_search_type = cast_away_optional(
+            first_non_none_value(search_run.query.cvd,
+                                 default=CvdSearchType.DEFAULT)
+        )
+
+        # For when you type 'cvd:exclusive' in a query to debug ONLY CVD results!
+        if cvd_search_type == CvdSearchType.EXCLUSIVE:
+
+            def sort_by_cvd(r: Result):
+                return r.cosine_vector_distance
+
+            search_run.sort_function = sort_by_cvd
+            do_cvd_search(search_run)
+            return search_run
+
+    fetch_results(search_run)
+
+    if (
+        settings.MORPHODICT_ENABLE_AFFIX_SEARCH
+        and include_affixes
+        and not query_would_return_too_many_results(search_run.internal_query)
+    ):
+        do_source_language_affix_search(search_run)
+        do_target_language_affix_search(search_run)
+
+    if settings.MORPHODICT_ENABLE_CVD:
+        if cvd_search_type.should_do_search() and not is_almost_certainly_cree(
+            search_run
+        ):
+            do_cvd_search(search_run)
+
+    if search_run.query.espt:
+        espt_search.inflect_search_results()
+
+    return search_run
