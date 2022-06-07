2b91d0bc1c3ca4714045cbefb4149c17a18a0af7
ansh010599 <ansh@ualberta.ca>
2022-06-07 13:36:39 -0600

    Legend abbr mostly working; need to add back button

--- general.py
+++ general.py
@@ -5,3 +5,16 @@ RELAXED_ANALYZER_FST_FILENAME = "crk-relaxed-analyzer-for-dictionary.hfstol"
 MORPHODICT_TAG_STYLE = "Bracket"
 
 SOUND_FILE_NAME = "sound.wav"
+
+LEGEND_OF_ABBREVIATIONS_TEXT = '''
+s/he, she, he
+[b]s[/b]he, [b]h[/b]e, or (singular) th[b]e[/b]y. Used in definitions to stand for the [b]animate actor[/b], which is mostly human but can sometimes refer to other animate entities as well. The approximate equivalent to the Cree propnoun wiya. (e.g. [i]"[b]s/he[/b] sees; [b]s/he[/b] sees something; [b]s/he[/b] sees someone"[/i])
+
+s.t., it
+[b]s[/b]ome[b]t[/b]hing. Used in definitions to stand for the [b]inanimate goal[/b]. (e.g. "[i]s/he sees [b]s.t.[/b], i.e. something; s/he sees [b]it[/b][/i]")
+
+s.o., her, him
+[b]s[/b]ome[b]o[/b]ne, but can also mean “something animate” like [b]pahkwêsikan[/b] or [b]asikan[/b]. Used in definitions to stand for the [b]animate goal[/b]. (e.g. "[i]s/he sees s.o., i.e. someone; s/he sees him; s/he sees her[/i]")
+it
+Used in definitions to stand for the [b]inanimate actor[/b] (e.g. "[i][b]it[/b] is blue[/i]") or [b]existential subject[/b] for impersonal verbs (the “it” in "[i]it is raining[/i]").
+'''
