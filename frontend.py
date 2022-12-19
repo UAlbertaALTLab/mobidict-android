91b15a51ce263d78963ba44855303c4a1d0da771
Ansh Dubey <ansh@ualberta.ca>
2022-12-18 22:51:58 -0700

    feat: modify pane separation algo to incorporate every enter being a new pane

--- frontend.py
+++ frontend.py
@@ -1040,6 +1040,7 @@ class SpecificResultMainList(MDList):
             current_num_cols = 0
             current_panes = []
             header = ""
+            isFirstPaneRow = True
             # Some panes have a header row and a subheader row like VTA
             # | Prs | Ind | Prs | Cnj ...
             # | 1Sg	| 1Sg | 1Sg | ...
@@ -1067,6 +1068,13 @@ class SpecificResultMainList(MDList):
                             continue
                         # If it does contain only column headers, let's add num_cols panes 
                         # as those = number of panes for that "pane" in this for loop
+                        if not isFirstPaneRow:
+                            for cell_idx, cell in enumerate(tr_row['cells']):
+                                if cell_idx != 0:
+                                    current_row = tr_row.copy()
+                                    current_row['cells'] = [cell]
+                                    current_panes[cell_idx - 1]['tr_rows'].append(current_row)
+                            continue
                         current_num_cols = num_cols
                         is_next_row_after_labels = True
                         for cell_idx, cell in enumerate(tr_row['cells']):
@@ -1076,6 +1084,7 @@ class SpecificResultMainList(MDList):
                                 currentHeader, currentSubheader = getHeaderAndSubheader(header, relabel(cell['label']), currentHeaderLabels, cell_idx)
                                 current_panes.append({'tr_rows': [], 'headerTitle': currentHeader, "subheaderTitle": currentSubheader})
                         # Let's look at the next row now that panes have been added
+                        isFirstPaneRow = False
                         continue
 
                     # Immitate the row minus the cells that we will just recreate
@@ -1102,17 +1111,17 @@ class SpecificResultMainList(MDList):
                                     # because they don't have only label row in middle of the output
                                     # so can be added in the end
                                     is_next_row_after_labels = False
-                                    for current_pane_idx in range(len(current_panes) - current_num_cols):
-                                        final_pane = {'pane': current_panes[current_pane_idx], 'header': current_panes[current_pane_idx]['headerTitle'], 'subheader': current_panes[current_pane_idx]['subheaderTitle']}
-                                        all_panes.append(final_pane)
+                                    # for current_pane_idx in range(len(current_panes) - current_num_cols):
+                                    #     final_pane = {'pane': current_panes[current_pane_idx], 'header': current_panes[current_pane_idx]['headerTitle'], 'subheader': current_panes[current_pane_idx]['subheaderTitle']}
+                                    #     all_panes.append(final_pane)
                                     
-                                    # Now that current panes so far have been added to all_panes,
-                                    # reset the current_panes list to just contain the current row (which is on -1 idx)
-                                    if len(current_panes) > 0:
-                                        current_panes_temp = current_panes.copy()
-                                        current_panes = list()
-                                        for i in range(current_num_cols, len(current_panes_temp)):
-                                            current_panes.append(current_panes_temp[i])
+                                    # # Now that current panes so far have been added to all_panes,
+                                    # # reset the current_panes list to just contain the current row (which is on -1 idx)
+                                    # if len(current_panes) > 0:
+                                    #     current_panes_temp = current_panes.copy()
+                                    #     current_panes = list()
+                                    #     for i in range(current_num_cols, len(current_panes_temp)):
+                                    #         current_panes.append(current_panes_temp[i])
                                 else:
                                     firstNonLabelRowInPane = False
                                     is_next_row_after_labels = False
